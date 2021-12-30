5cbdd15eeaf28bed76fa9219cc60ff441fc0c225
Shchegolevvadim <90051320+Shchegolevvadim@users.noreply.github.com>
2021-12-30 16:06:17 +0300

    Create Method

--- итоговая/Program.cs
+++ итоговая/Program.cs
@@ -1,1 +1,37 @@
-﻿int[] array = {5, 6, 7, 10, 16, 19, 23, 45};
+﻿int[] CreateArray(int count)
+{
+    return new int[count];
+}
+void FillArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i]= new Random().Next(0,10000);
+    }
+}
+string PrintArray2(int[] array)
+{
+    int count = array.Length;
+    string res =String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        res += $"{array[i]} ";
+    }
+    return res;
+}
+void Method(int[] array)
+{ for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] %2 == 0) 
+    {Console.WriteLine(array[i]);}
+}
+
+{
+    
+}}
+int[] arr = CreateArray(8);
+FillArray(arr);
+Console.WriteLine(PrintArray2(arr));
+Console.WriteLine($"->");
+Method(arr);
